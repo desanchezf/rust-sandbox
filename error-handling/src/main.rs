361b8b8371154364ef88ba0b8efd307dbbb23c09
David Sánchez <davidsfernandez7@outlook.es>
2025-10-09 16:29:36 +0200

    Update

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -9,7 +9,7 @@ use std::io::ErrorKind;
 
 
 fn main() {
-    // panic!("Crash and burn!"); 
+    // panic!("Crash and burn!");
 
     // thread 'main' panicked at src\main.rs:2:5:
     // Crash and burn!
@@ -18,7 +18,7 @@ fn main() {
 
     //let v = vec![1, 2, 3];
     //v[99];
-    
+
     // thread 'main' panicked at src\main.rs:10:6:
     // index out of bounds: the len is 3 but the index is 99
     // note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
@@ -26,7 +26,7 @@ fn main() {
 
     // Esto sirve para ver el conjunto de funciones que se han llamado hasta ese punto
     // RUST_BACKTRACE=1
-    
+
     // -> Result<File, std::io::Error>
     // let greeting_file = File::open("hello.txt");
     // let greeting_file_result = match greeting_file {
@@ -49,7 +49,7 @@ fn main() {
     };
     // println!("El valor de hello_file_result es: {:?}", hello_file_result); --> Irrelevante retorna el APTH
 
-    let greeting_file_result = File::open("hello-error.txt");
+    let greeting_file_result = File::open("src/hello-error.txt");
 
     let greeting_file = match greeting_file_result {
         Ok(file) => file,
@@ -64,11 +64,11 @@ fn main() {
         },
     };
 
-    // Esto se puede hacer de la siguiente manera: 
+    // Esto se puede hacer de la siguiente manera:
 
-    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+    let greeting_file = File::open("src/hello.txt").unwrap_or_else(|error| {
         if error.kind() == ErrorKind::NotFound {
-            File::create("hello.txt").unwrap_or_else(|error| {
+            File::create("src/hello.txt").unwrap_or_else(|error| {
                 panic!("Problem creating the file: {error:?}");
             })
         } else {
@@ -76,10 +76,4 @@ fn main() {
         }
     });
 
-
-
-    
-    
-
-    
 }
